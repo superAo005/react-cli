c5e8ef9bd5cacef932ca4bf9c452fd69d63d1511
zhenwangYang <zhenwangyang@trip.com>
2022-09-26 19:18:21 +0800

    feat：优化

--- log/footer.js
+++ log/footer.js
@@ -4,7 +4,7 @@ const ConsoleLog = require('../src/utils/console')
 const gitUrl = require('../src/utils/gitUrl')
 const ConsoleFoot = () => {
   ConsoleLog(gradient.cristal('  cli: https://github.com/superAo005/react-cli') + '\n')
-  ConsoleLog(gradient.rainbow(`模版地址: https://github.com/superAo005/react-vite ') + '\n'`)
+  ConsoleLog(gradient.rainbow(`模版地址: ${gitUrl}`) + '\n')
 }
 
 module.exports = ConsoleFoot
