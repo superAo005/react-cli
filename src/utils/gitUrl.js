30310b4f5f7bf0d8645e7e4c23028fa922698daf
zhenwangYang <zhenwangyang@trip.com>
2022-10-13 17:19:25 +0800

    feat:优化

--- src/utils/gitUrl.js
+++ src/utils/gitUrl.js
@@ -3,8 +3,9 @@
  * @Date 2022/3/9
  * @Version 1.0.0
  * @Last Modified by superAo
- * @Last Modified Time 2022/3/9
+ * @Last Modified Time 2022/10/13
  */
-const gitUrl = 'https://github.com/superAo005/react-vite'
+const gitUrlHttp = 'https://github.com/superAo005/react-vite.git'
+const gitUrl = 'git@github.com:superAo005/react-vite.git'
 // const gitUrl = 'https://gitee.com/fillter/react-vite'
 module.exports = gitUrl
